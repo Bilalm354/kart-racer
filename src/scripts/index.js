bdfe3aec7c7b6b9fd7afef634c5e6db69891b45a
bilalm354 <bilalm354@gmail.com>
2020-03-24 10:27:56 +0000

    commented out GTFL loader

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -65,31 +65,31 @@ camera.position.z = 20;
 // var _controls = new OrbitControls(camera, renderer.domElement);
 // console.log(controls);
 
-var loader = new THREE.GLTFLoader();
+// var loader = new THREE.GLTFLoader();
 
-// Load a glTF resource
-loader.load(
-    // resource URL
-    "models/gltf/duck/duck.gltf",
-    // called when the resource is loaded
-    function(gltf) {
-        scene.add(gltf.scene);
+// // Load a glTF resource
+// loader.load(
+//     // resource URL
+//     "models/gltf/duck/duck.gltf",
+//     // called when the resource is loaded
+//     function(gltf) {
+//         scene.add(gltf.scene);
 
-        gltf.animations; // Array<THREE.AnimationClip>
-        gltf.scene; // THREE.Group
-        gltf.scenes; // Array<THREE.Group>
-        gltf.cameras; // Array<THREE.Camera>
-        gltf.asset; // Object
-    },
-    // called while loading is progressing
-    function(xhr) {
-        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-    },
-    // called when loading has errors
-    function(error) {
-        console.log("An error happened");
-    }
-);
+//         gltf.animations; // Array<THREE.AnimationClip>
+//         gltf.scene; // THREE.Group
+//         gltf.scenes; // Array<THREE.Group>
+//         gltf.cameras; // Array<THREE.Camera>
+//         gltf.asset; // Object
+//     },
+//     // called while loading is progressing
+//     function(xhr) {
+//         console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+//     },
+//     // called when loading has errors
+//     function(error) {
+//         console.log("An error happened");
+//     }
+// );
 
 var animate = function() {
     requestAnimationFrame(animate);
