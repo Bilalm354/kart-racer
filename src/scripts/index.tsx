3d1749d427a3378ba35c6c5c26abaaabdaa07295
bilalm354 <bilalm354@gmail.com>
2020-09-23 22:45:50 +0100

    better

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -15,8 +15,8 @@ import { keyboard } from "./data/keyboard";
 import { camera } from "./three/camera";
 import { ambientLight } from "./three/ambientLight";
 import { directionalLight } from "./three/directionalLight";
-import { Options } from "./react/Play";
-import { Object3D, Vector3 } from "three";
+import { Menu } from "./ui/Menu";
+import { Object3D } from "three";
 
 document.addEventListener("keydown", (event) =>
     keyDownHandler(event, keyboard)
@@ -34,18 +34,6 @@ camera.position.set(0, 100, 100);
 
 scene.background = new THREE.Color(0xfad6a5);
 
-// maybe world should be a class. with an array of threeJs objects
-
-// function updateWorld() {
-
-// }
-
-interface things {
-    id: number,
-    object3d: Object3D,
-    location: Vector3
-} /// lets go baby 
-
 const objects: Object3D[] = [track, car, ambientLight, directionalLight] // could make it an array of objects with a id, 3d location and 3dobject.
 
 function init() {
@@ -55,48 +43,6 @@ function init() {
     camera.up.set(0, 0, 1);
 }
 init();
-
-export function unmute() {
-// instantiate a listener
-const audioListener = new THREE.AudioListener();
-
-// add the listener to the camera
-camera.add( audioListener );
-
-// instantiate audio object
-const oceanAmbientSound = new THREE.Audio( audioListener );
-
-// add the audio object to the scene
-scene.add( oceanAmbientSound );
-
-// instantiate a loader
-const loader = new THREE.AudioLoader();
-
-// load a resource
-loader.load(
-	// resource URL
-	'audio/aliensExist.mp3',
-
-	// onLoad callback
-	function ( audioBuffer ) {
-		// set the audio object buffer to the loaded object
-		oceanAmbientSound.setBuffer( audioBuffer );
-
-		// play the audio
-		oceanAmbientSound.play();
-	},
-
-	// onProgress callback
-	function ( xhr ) {
-		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-	},
-
-	// onError callback
-	function ( err ) {
-		console.log( 'An error happened' );
-	}
-);
-}
 
 function updateSceneAndCamera() {
     // create an AudioListener and add it to the camera
