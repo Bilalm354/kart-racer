ec1a4f9771ed61db25e53b73f7d2e0736898caed
bilalm354 <bilalm354@gmail.com>
2020-09-30 15:25:00 +0100

    refactored collsion detection

--- src/engine/World.ts
+++ src/engine/World.ts
@@ -1,6 +1,6 @@
 import {
   Group, Scene, Object3D, Light, Camera, Color, WebGLRenderer,
-  PerspectiveCamera, Mesh, BoxHelper, Box3,
+  PerspectiveCamera, Mesh, Box3,
 } from 'three';
 import { Car } from '~bodies/vehicles/Car.ts';
 import { ambientLight, directionalLight } from '~/misc/lights.ts';
@@ -20,9 +20,10 @@ export class World {
   private cameraView: CameraView;
   private renderer: WebGLRenderer
   private newCube: Mesh;
-  private carBoundingBoxHelper: BoxHelper
   private carBoundingBox: Box3;
   private newCubeBoundingBox: Box3;
+  private collidableBoundingBoxes: Box3[];
+
   constructor() {
     this.scene = new Scene();
     this.camera = new PerspectiveCamera(
@@ -36,39 +37,41 @@ export class World {
     this.otherObjects = [];
     this.cameraView = 'behindCar';
     this.newCube = newCube();
-    this.carBoundingBoxHelper = new BoxHelper(this.car.object3d, 'white');
     this.carBoundingBox = new Box3().setFromObject(this.car.object3d);
     this.newCubeBoundingBox = new Box3().setFromObject(this.newCube);
+    this.collidableBoundingBoxes = [this.newCubeBoundingBox];
   }
 
-  resolveCollision(a:Box3, b:Box3) {
-    console.log(`resolve collision between ${a} and ${b}`);
+  private resolveCollision(box: Box3) {
+    console.log(`resolve collision between ${this.car} and ${box}`);
   }
 
-  collisionCheck(a: Box3, b:Box3) {
-    if ((a.intersectsBox(b))) {
-      console.log('colliding');
-      this.resolveCollision(a, b);
-    }
+  private collisionCheck(collidableBoxes: Box3[]) {
+    collidableBoxes.forEach((collidableBox) => {
+      if ((this.carBoundingBox.intersectsBox(collidableBox))) {
+        console.log('colliding');
+        this.resolveCollision(collidableBox);
+      }
+    });
   }
 
-  onWindowResize() {
+  public onWindowResize() {
     this.camera = new PerspectiveCamera(
       75, window.innerWidth / window.innerHeight, 0.1, 2000,
     );
     this.renderer.setSize(window.innerWidth, window.innerHeight);
   }
 
-  addBox(object: Object3D) {
+  public addBox(object: Object3D) {
     this.otherObjects.push(object);
   }
 
-  addCar() {
+  public addCar() {
     this.otherObjects.push(new Car().object3d);
     this.updateSceneContents();
   }
 
-  init() {
+  public init() {
     this.scene.background = new Color(0xfad6a5);
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(this.renderer.domElement);
@@ -76,35 +79,39 @@ export class World {
     this.car.object3d.position.set(0, 0, 3);
     directionalLight.position.set(1, 1, 0.5).normalize();
     this.scene.add(this.track, this.ambientLight, this.directionalLight,
-      this.car.object3d, this.newCube, this.carBoundingBoxHelper);
+      this.car.object3d, this.newCube);
     this.camera.up.set(0, 0, 1);
     this.newCube.position.set(0, 50, 5);
   }
 
-  uninit() {
+  public uninit() {
     this.scene.remove(this.car.object3d);
   }
 
-  updateSceneContents() {
+  public updateSceneContents() {
     this.scene.remove(this.track, this.car.object3d,
       ...this.otherObjects, this.ambientLight, this.directionalLight);
     this.scene.add(this.track, this.car.object3d,
       ...this.otherObjects, this.ambientLight, this.directionalLight);
   }
 
-  updateSceneAndCamera() {
+  public updateBoundingBoxes() {
+    this.carBoundingBox = new Box3().setFromObject(this.car.object3d);
+    this.newCubeBoundingBox = new Box3().setFromObject(this.newCube);
+    this.collidableBoundingBoxes = [this.newCubeBoundingBox];
+  }
+
+  public updateSceneAndCamera() {
     this.setCameraPosition(this.cameraView);
     this.car.updateKeyboard(keyboard);
     this.car.update();
     this.car.updateObject3d();
-    this.carBoundingBoxHelper.update();
-    this.carBoundingBox = new Box3().setFromObject(this.car.object3d);
-    this.newCubeBoundingBox = new Box3().setFromObject(this.newCube);
-    this.collisionCheck(this.carBoundingBox, this.newCubeBoundingBox);
+    this.updateBoundingBoxes();
+    this.collisionCheck(this.collidableBoundingBoxes);
     this.renderer.render(this.scene, this.camera);
   }
 
-  setCameraPosition(view: CameraView) {
+  private setCameraPosition(view: CameraView) {
     if (view === 'top') {
       this.camera.position.set(0, 0, 300);
       this.camera.lookAt(0, 0, 0);
@@ -114,16 +121,16 @@ export class World {
     }
   }
 
-  setCameraView(view: CameraView) {
+  public setCameraView(view: CameraView) {
     this.cameraView = view;
   }
 
-  setSmallTrack(): void {
+  public setSmallTrack(): void {
     this.scene.remove(bigTrack);
     this.scene.add(smallTrack);
   }
 
-  setBigTrack(): void {
+  public setBigTrack(): void {
     this.scene.remove(smallTrack);
     this.scene.add(bigTrack);
   }
