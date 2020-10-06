2e3d694ed34c13a6b4fa04289555359b594325a9
bilalm354 <bilalm354@gmail.com>
2020-10-06 21:26:19 +0100

    fixes #105

--- client/src/misc/touchHandler.ts
+++ client/src/misc/touchHandler.ts
@@ -2,23 +2,27 @@ import { keyboard } from './Keyboard';
 
 export function handleStart(e: TouchEvent) {
   if (e.touches) {
+    e.preventDefault();
     keyboard.up = true;
   }
 }
 
 export function handleEnd(e: TouchEvent) {
   if (e.touches) {
+    e.preventDefault();
     keyboard.up = false;
   }
 }
 
 export function handleCancel(e: TouchEvent) {
   if (e.touches) {
+    e.preventDefault();
     keyboard.up = false;
   }
 }
 
 export function handleMove(e:TouchEvent) {
+  e.preventDefault();
   console.log(e);
   console.log('handleMove');
 }
