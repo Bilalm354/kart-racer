07e79411882666bf7f3ff784f86fd1793babfc82
bilalm354 <bilalm354@gmail.com>
2020-10-14 00:13:31 +0100

    started to make Cube and Track instead of TrackCreator

---
+++ client/src/bodies/Cube.ts
@@ -0,0 +1,7 @@
+import { BoxGeometry, MeshStandardMaterial, Mesh } from "three";
+
+export function Cube(cubeLength: number): Mesh {
+    const geometry = new BoxGeometry(cubeLength, cubeLength, cubeLength);
+    const material = new MeshStandardMaterial({ color: 0xff0000 });
+    return new Mesh(geometry, material)
+}
---
+++ client/src/bodies/Track.ts
@@ -0,0 +1,12 @@
+import { Group, Mesh, Vector3 } from "three"
+
+export class Track {
+    ground: Mesh[];
+    walls: Group[];
+    startingPosition: Vector3;
+    constructor() {
+        this.ground = [];
+        this.walls = [];
+        this.startingPosition = new Vector3();
+}
+}
