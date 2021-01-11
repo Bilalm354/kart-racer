fa05acec73735b4b9a1a9bb05eaffb25b11154e3
bilalm354 <bilalm354@gmail.com>
2021-01-11 00:46:25 +0000

    edits to comments

--- client/src/ui/Menu.tsx
+++ client/src/ui/Menu.tsx
@@ -29,7 +29,7 @@ export const Menu = () => {
         <form>
           <button type="button" onClick={() => world.setMode('play')}>Play Mode</button>
           <button type="button" onClick={() => world.setMode('create')}>Create Mode (click while mouse is on the ground)</button>
-          {true ? undefined : (<button type="button" onClick={() => alert('Paused!')}>Pause</button>)}
+          <button type="button" onClick={() => alert('Paused!')}>Pause</button>
           <button type="button" onClick={() => world.setSmallTrack()}>Small Track</button>
           <button type="button" onClick={() => world.setBigTrack()}>Large Track</button>
           <button type="button" onClick={() => setShowLeaderboard(!showLeaderboard)}>See Leaderboard</button>
