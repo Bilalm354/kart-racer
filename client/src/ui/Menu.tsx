fd8a29b5f721388a2edaadc823212f4e216a1b31
bilalm354 <bilalm354@gmail.com>
2021-01-13 00:47:11 +0000

    Added react bootstrap navbar to Menu.tsx instead of buttons. Should be mobile responsive.

--- client/src/ui/Menu.tsx
+++ client/src/ui/Menu.tsx
@@ -1,6 +1,8 @@
 // eslint-disable-next-line no-use-before-define
 import React, { useState } from 'react';
-import { Container, Row } from 'react-bootstrap';
+import {
+  Container, Nav, Navbar, NavDropdown, Row,
+} from 'react-bootstrap';
 import ProgressBar from 'react-bootstrap/esm/ProgressBar';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { world } from '~/World';
@@ -22,27 +24,41 @@ export const Menu = () => {
   const [showLeaderboard, setShowLeaderboard] = useState(false);
   const [showAddScore, setShowAddScore] = useState(false);
   const [showTouchpad, setShowTouchpad] = useState(isMobile);
-
+  // TODO: stop stats.js from getting in the way of the first nav bar option
   return (
     <ApolloProvider client={client}>
       <Container fluid className="p-0">
-        <form>
-          <button type="button" onClick={() => world.setMode('play')}>Play Mode</button>
-          <button type="button" onClick={() => world.setMode('create')}>Create Mode (click while mouse is on the ground)</button>
-          <button type="button" onClick={() => alert('Paused!')}>Pause</button>
-          <button type="button" onClick={() => world.setSmallTrack()}>Small Track</button>
-          <button type="button" onClick={() => world.setBigTrack()}>Large Track</button>
-          <button type="button" onClick={() => setShowLeaderboard(!showLeaderboard)}>See Leaderboard</button>
-          <button type="button" onClick={() => setShowAddScore(!showAddScore)}>Add Score</button>
-          <button type="button" onClick={() => setShowTouchpad(!showTouchpad)}>Show/ Hide Touchpad</button>
-          <button type="button" onClick={() => audioElement.play()}>Play Music</button>
-          <button type="button" onClick={() => audioElement.pause()}>Pause Music</button>
-          <button type="button" onClick={() => world.setCameraView('top')}>Top Camera</button>
-          <button type="button" onClick={() => world.setCameraView('behindCar')}>Behind Car Camera</button>
-          <button type="button" onClick={() => world.car.setColor('red')}>Red Car</button>
-          <button type="button" onClick={() => world.car.setColor('green')}>Green Car</button>
-          <button type="button" onClick={() => world.car.setColor('blue')}>Blue Car</button>
-        </form>
+        <Navbar bg="dark" expand="lg">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <NavDropdown title="Mode" id="collasible-nav-dropdown">
+              <Nav.Link onClick={() => world.setMode('play')}>Play</Nav.Link>
+              <Nav.Link onClick={() => world.setMode('create')}>Create (click while mouse is on the ground)</Nav.Link>
+            </NavDropdown>
+            <NavDropdown title="Track" id="collasible-nav-dropdown">
+              <Nav.Link onClick={() => world.setSmallTrack()}>Small Track</Nav.Link>
+              <Nav.Link onClick={() => world.setBigTrack()}>Large Track</Nav.Link>
+            </NavDropdown>
+            <NavDropdown title="Leaderboard" id="collasible-nav-dropdown">
+              <Nav.Link onClick={() => setShowLeaderboard(!showLeaderboard)}>See Leaderboard</Nav.Link>
+              <Nav.Link onClick={() => setShowAddScore(!showAddScore)}>Add Score</Nav.Link>
+            </NavDropdown>
+            <NavDropdown title="Music" id="collasible-nav-dropdown">
+              <Nav.Link onClick={() => audioElement.play()}>Play Music</Nav.Link>
+              <Nav.Link onClick={() => audioElement.pause()}>Pause Music</Nav.Link>
+            </NavDropdown>
+            <NavDropdown title="Camera" id="collasible-nav-dropdown">
+              <Nav.Link onClick={() => world.setCameraView('top')}>Top Camera</Nav.Link>
+              <Nav.Link onClick={() => world.setCameraView('behindCar')}>Behind Car Camera</Nav.Link>
+            </NavDropdown>
+            <NavDropdown title="Car Colour" id="collasible-nav-dropdown">
+              <Nav.Link onClick={() => world.car.setColor('red')}>Red</Nav.Link>
+              <Nav.Link onClick={() => world.car.setColor('green')}>Green</Nav.Link>
+              <Nav.Link onClick={() => world.car.setColor('blue')}>Blue</Nav.Link>
+            </NavDropdown>
+            <Nav.Link onClick={() => setShowTouchpad(!showTouchpad)}>Show/ Hide Touchpad</Nav.Link>
+          </Navbar.Collapse>
+        </Navbar>
         {showLeaderboard ? <Leaderboard /> : undefined}
         {showAddScore ? <AddScore /> : undefined}
         <Container fluid className="fixed-bottom text-center">
